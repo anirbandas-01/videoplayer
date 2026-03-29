08fc6b2761fbf28de4226b06be71828137f5ac0d
Anirban Das <anirban140963@gmail.com>
2026-03-29 20:27:38 +0530

    feat: add user authentication with login and JWT verification; include bcryptjs dependency

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -49,11 +49,66 @@ const registerUser = async (req, res) => {
             },
            });
     } catch (error) {
-        console.error(500).json({
+        console.error(error);
+        return res.status(500).json({
             success: false,
             message: "server error"
         });
     }
 };
 
-export  { registerUser };
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and password required",
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message: "user not found",
+            });
+        }
+
+        const isMatch = await user.isPasswordCorrect(password);
+
+        if(!isMatch) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid credentials"
+            });
+        }
+
+        const token = jwt.sign(
+            { id:user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "7d" }
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "Login successfully",
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Server error",
+        });
+    }
+};
+
+export  { registerUser, loginUser };
