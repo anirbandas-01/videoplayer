7b2e4d3597b569597381fe043824a508aaa9dd55
Anirban Das <anirban140963@gmail.com>
2026-04-01 20:46:38 +0530

    feat: import dotenv and configure environment variables

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -9,6 +12,7 @@ app.use(cors( {
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
